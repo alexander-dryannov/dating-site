0df1d1ad3d1978820083e8e5880ece5dcf08e38b
alexander-dryannov <l3x.d@yandex.ru>
2021-12-01 01:47:09 +0300

    Fixed filters.

--- looking_my_love/profiles/filters.py
+++ looking_my_love/profiles/filters.py
@@ -4,8 +4,8 @@ from .models import User
 
 class UserListFilter(filters.FilterSet):
     gender = filters.CharFilter(field_name='gender')
-    first_name = filters.CharFilter(field_name='first_name')
-    last_name = filters.CharFilter(field_name='last_name')
+    fname = filters.CharFilter(field_name='first_name')
+    lname = filters.CharFilter(field_name='last_name')
 
     class Meta:
         model: User
