90e25d6c5acfe4d85688fffb00fc13df840561cb
alexander-dryannov <l3x.d@yandex.ru>
2021-12-05 15:55:27 +0300

    Fix deploy.

--- looking_my_love/config/gunicorn.conf.py
+++ looking_my_love/config/gunicorn.conf.py
@@ -1,6 +1,4 @@
-import os
-
 bind = '127.0.0.1:8000'
 workers = 2
-user = os.getlogin()
+user = 'current_user'
 timeout = 120
--- looking_my_love/looking_my_love/settings.py
+++ looking_my_love/looking_my_love/settings.py
@@ -37,6 +37,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    # 'DEFAULT_RENDERER_CLASSES': [
+    #     'rest_framework.renderers.JSONRenderer',
+    # ],
 }
 
 MIDDLEWARE = [
